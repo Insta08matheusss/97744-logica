eee374d6e59b4988b62401d3820cc0a0397370b5
Insta08matheusss <mtfranca06@gmail.com>
2025-02-20 19:54:54 +0000

    Salvar

---
+++ Fiz em casa /Exercicio.py
@@ -0,0 +1,14 @@
+import os 
+
+os.system ("clear") 
+
+
+idade = float(input("Digite sua idade: "))
+
+if idade >= 18 and idade < 65:
+    print("Você é obrigado da votarr")
+
+else:
+    print("Você não é obrigado a votar")
+
+  
---
+++ Fiz em casa /exercicio1.py
@@ -0,0 +1,25 @@
+import os 
+
+os.system ("clear") 
+
+idade = float(input("Digite sua idade: "))
+
+if idade <= 15:
+    print("Não podem votar.")
+
+elif idade >= 16 and idade <= 17:
+    print("Voto Opcional.")
+
+else:
+    idade >= 18 and idade < 65
+    print("Você é obrigado da votar")
+
+
+
+    
+
+
+
+
+     
+

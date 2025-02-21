1c56092edb49ef28d2a3a7d2f3f03e3306d1b39b
Insta08matheusss <mtfranca06@gmail.com>
2025-02-21 19:58:31 +0000

    Salvar

--- Fiz em casa /exercicio2 .py
+++ Fiz em casa /exercicio2 .py
@@ -4,11 +4,12 @@ os.system ("clear")
 
 primeiro_numero = int(input("Digite um número: "))
 segundo_numero = int(input("Digite outro número: "))
+terceiro_numero = int(input("Digite o terceiro número: "))
 
-maior_numero = max(primeiro_numero, segundo_numero)
-menor_numero = min(primeiro_numero, segundo_numero)
+maior_numero = max(primeiro_numero, segundo_numero, terceiro_numero)
+menor_numero = min(primeiro_numero, segundo_numero, terceiro_numero)
 
-if primeiro_numero ==  segundo_numero:
+if primeiro_numero ==  segundo_numero == terceiro_numero:
     print(f"Os números são iguais")
 else:
     print (f"Maior número: {maior_numero}")
---
+++ cardapio.py
@@ -0,0 +1,45 @@
+import os
+
+os.system("clear")
+print("""
+
+=========== MENU ==========
+Código   \tPrato        \tValor
+1         \tFeijoada      \t50,00
+2          \tLasanha      \t70,00
+3           \tStrogonoff   \t15,00
+4            \tLasanha  \t 15,00
+6             \tEscondidinho  \t30,00
+
+""")
+opcao = int(input("Digete sua opção desejada: "))
+
+match opcao:
+    case 1 :
+       prato = "Feijoada"
+       valor = 50,00
+    case 2 :
+       prato = "Lasanha"
+       valor = 70
+    case 3:
+         prato = "Strogonoff"
+         valor = 15,00
+    case 4:
+        prato = "Lasanha"
+        valor = 15,00
+    case 5:
+        prato = "Escondidinho"
+        valor = 30,00
+
+print()
+print(f"Prato: {prato}")
+print(F"Valor: R$ {valor: .2f}")
+
+    
+
+
+    
+
+
+
+ 
